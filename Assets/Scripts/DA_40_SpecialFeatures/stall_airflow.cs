34f7acae026c1975e9ce1d60b61c9880bec5bbe3
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2026-05-21 14:47:10 +0930

    fix ghost trail

--- Assets/Scripts/DA_40_SpecialFeatures/stall_airflow.cs
+++ Assets/Scripts/DA_40_SpecialFeatures/stall_airflow.cs
@@ -1,46 +1,57 @@
-using System.Collections;
 using UnityEngine;
 
 public class StallVisualTrigger : MonoBehaviour
 {
     [SerializeField] private DA_40 aircraft;
     [SerializeField] private ParticleSystem ghostTrail;
-    [SerializeField] private float sinkRate = 4f;
+    
+    // Instantly injects this much upward speed the moment a ghost spawns
+    [SerializeField] private float initialUpwardSpeed = 40f; 
+    
+    // How fast it continues to curve up into the sky over time
+    [SerializeField] private float upwardAcceleration = 20f; 
 
     private ParticleSystem.Particle[] particles;
 
     public void OnStallBegin()
     {
         aircraft.EnableGhostTrail(true);
-        StartCoroutine(ApplyStallMotion());
     }
 
     public void OnStallEnd()
     {
-        StopAllCoroutines();
         aircraft.EnableGhostTrail(false);
     }
 
-    private IEnumerator ApplyStallMotion()
+    private void LateUpdate()
     {
-        particles = new ParticleSystem.Particle[ghostTrail.main.maxParticles];
+        if (ghostTrail == null || ghostTrail.particleCount == 0) return;
 
-        while (true)
+        if (particles == null || particles.Length < ghostTrail.main.maxParticles)
         {
-            int count = ghostTrail.GetParticles(particles);
-
-            for (int i = 0; i < count; i++)
-            {
-                float age = 1f - (particles[i].remainingLifetime / particles[i].startLifetime);
-
-                if (age < 0.5f)
-                    particles[i].velocity += Vector3.up * 1.5f * Time.deltaTime;
-                else
-                    particles[i].velocity += Vector3.down * sinkRate * Time.deltaTime;
-            }
-
-            ghostTrail.SetParticles(particles, count);
-            yield return null;
+            particles = new ParticleSystem.Particle[ghostTrail.main.maxParticles];
         }
+
+        int count = ghostTrail.GetParticles(particles);
+
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 vel = particles[i].velocity;
+
+            // 1. INSTANT TOP-RIGHT ANGLE:
+            // Since the horizontal speed is ~50, we force the Y speed to roughly match it.
+            // This instantly creates a 45-degree angle to the top-right!
+            if (vel.y < initialUpwardSpeed) 
+            {
+                vel.y = initialUpwardSpeed; 
+            }
+            
+            // 2. Continually add curve so it swoops up beautifully
+            vel.y += upwardAcceleration * Time.deltaTime; 
+            
+            particles[i].velocity = vel;
+        }
+
+        ghostTrail.SetParticles(particles, count);
     }
 }
