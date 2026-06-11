d56d74513e99fc56548618d02b6b3ff7b35ad596
Ian505 <114987164+Ian505@users.noreply.github.com>
2026-06-12 02:04:01 +0930

    adding comments

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -2,20 +2,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+// start menu controlled. scene selection empty 
 public class StartMenu : MonoBehaviour
 {
+    //the play button in inspector
     [SerializeField] private Button playButton;
 
     private string selectedScene = "";
 
     void Start()
     {
+        //when game is started, play button grayed out until scene chosen
         if (playButton != null)
         {
             playButton.interactable = false;
         }
     }
 
+    //linked to tut button. once tut module clicekd the play button clickable
     public void SelectTutorial()
     {
         selectedScene = "v1.2";
@@ -26,6 +30,7 @@ public class StartMenu : MonoBehaviour
         }
     }
 
+    //same as tut but for spinning stalling module
     public void SelectSpinningStalling()
     {
         selectedScene = "MainVRScene";
@@ -36,6 +41,7 @@ public class StartMenu : MonoBehaviour
         }
     }
 
+    //do nothing if no scene is selected, if selected load the selected scene
     public void PlaySelectedScene()
     {
         if (string.IsNullOrEmpty(selectedScene))
